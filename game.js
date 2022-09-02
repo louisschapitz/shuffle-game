427ead1221bca4d790afd4aa2436dfe8770ac7d3
louisschapitz <louis.schapitz@gmail.com>
2022-09-02 10:23:21 +0200

    First try - sliding items

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 // Difficulty
 
-const difficulty_input = document.querySelector(".difficulty input");
-const difficulty_output = document.querySelector(".difficulty .output");
+const difficulty_input = document.querySelector(".game-container .difficulty input");
+const difficulty_output = document.querySelector(".game-container .difficulty .output");
 
 const difficulty_names = ["easy", "medium", "hard"];
 
 const difficulty = () => {
-    document.querySelector(".difficulty .output").innerText = difficulty_names[difficulty_input.value];
+    document.querySelector(".game-container .difficulty .output").innerText = difficulty_names[difficulty_input.value];
 };
 
 difficulty();
@@ -16,40 +16,70 @@ difficulty_input.addEventListener('input', difficulty);
 
 const items_container = document.querySelector('.game-container .items');
 
+function new_pattern(items) {
+    items_container.innerHTML = "";
+    items.forEach(item => {
+        items_container.innerHTML += `<div class="item item-${item}">${item}</div>`;
+    });
+}
+
 function start() {
     const max_items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     const random_items = max_items.sort((a, b) => 0.5 - Math.random());
 
-    items_container.innerHTML = "";
-    random_items.forEach(item => {
-        items_container.innerHTML += `<div class="item item-${item}">${item}</div>`;
-    });
+    new_pattern(random_items);
 
     items_container.addEventListener('click', (e) => {
-        let empty_field_position = random_items.indexOf(9);
 
         if (e.target.classList.contains('item')) {
 
-            let clicked = e.target.innerText;
+            let empty_field_position = random_items.indexOf(9);
+            let clicked = random_items.indexOf(Number(e.target.innerText));
             let switch_allowed = [];
-            
+
             if (empty_field_position === 0) {
-                console.log(random_items[1], random_items[3]);
+                switch_allowed.push(1, 3);
             }
 
             if (empty_field_position === 1) {
-                console.log(random_items[0], random_items[2], random_items[4]);
+                switch_allowed.push(0, 2, 4);
             }
 
             if (empty_field_position === 2) {
-                console.log(random_items[1], random_items[5]);
-            }
-            
-            if (empty_field_position === 3) {
-                console.log(random_items[0], random_items[4], random_items[6]);
+                switch_allowed.push(1, 5);
             }
 
+            if (empty_field_position === 3) {
+                switch_allowed.push(0, 4, 6);
+            }
+
+            if (empty_field_position === 4) {
+                switch_allowed.push(1, 3, 5, 7);
+            }
+
+            if (empty_field_position === 5) {
+                switch_allowed.push(2, 4, 8);
+            }
+
+            if (empty_field_position === 6) {
+                switch_allowed.push(3, 7);
+            }
+
+            if (empty_field_position === 7) {
+                switch_allowed.push(6, 4, 8);
+            }
+
+            if (empty_field_position === 8) {
+                switch_allowed.push(5, 7);
+            }
+
+            console.log(`clicked: ${clicked}, empty_field_position: ${empty_field_position}`);
+
+            if (switch_allowed.includes(clicked)) {
+                [random_items[empty_field_position], random_items[clicked]] = [random_items[clicked], random_items[empty_field_position]];
+                new_pattern(random_items);
+            }
 
         }
     });
@@ -57,7 +87,7 @@ function start() {
 
 
 }
-start();
 
+const start_button = document.querySelector('.game-container .menu .start');
 
-
+start_button.addEventListener('click', start);
