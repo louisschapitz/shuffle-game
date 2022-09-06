1174ab4dbb1fd8d8f6fb6e4e6d7e77e1298b7f0e
louisschapitz <louis.schapitz@gmail.com>
2022-09-06 14:33:58 +0200

    Image Slide Overlay Fix
    Better GUI

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const all_items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let current_items;
 function random_items() {
     current_items = all_items.slice().sort((a, b) => 0.5 - Math.random());
-    current_items = [1, 2, 3, 4, 5, 9, 7, 8, 6];
+    // current_items = [1, 2, 3, 4, 5, 9, 7, 8, 6]; // Easy
     return current_items;
 }
 
@@ -17,14 +17,17 @@ const states = ["medium", "hard", "easy"]; // 4x4 | 5x5 | 3x3
 let difficulty_value = 0;
 btn_difficulty.addEventListener('click', () => {
     if (game_status == 0) {
-        btn_difficulty.innerText = states[difficulty_value];
+        let difficulty_value_text = states[difficulty_value];
+        btn_difficulty.innerText = difficulty_value_text;
         difficulty_value = (difficulty_value + 1) % states.length;
-    
-        if (difficulty_value > 0) {
+
+        if (difficulty_value == 2) {
             game_container.classList.add('game--img');
+            btn_difficulty.classList.add('game__btn--hard');
         }
         else {
             game_container.classList.remove('game--img');
+            btn_difficulty.classList.remove('game__btn--hard');
         }
     }
 });
@@ -36,6 +39,21 @@ function new_pattern(items) {
     });
 }
 
+function correctMarker() {
+    for (let i = 0; i < current_items.length; i++) {
+        if (current_items[i] == all_items[i]) {
+            document.querySelector('.game__item--' + current_items[i]).classList.add('game__item--correct');
+        }
+    }
+}
+
+function btnVisibility() {
+    start_button.classList.toggle('game__btn--disabled');
+    restart_button.classList.toggle('game__btn--disabled');
+    items_container.classList.toggle('game__board--disabled');
+    btn_difficulty.classList.toggle('game__btn--disabled');
+}
+
 start_button.addEventListener('click', () => {
     if (start_button.classList.contains('game__btn--disabled')) {
         return;
@@ -43,10 +61,8 @@ start_button.addEventListener('click', () => {
     else {
         game_status = 1;
         new_pattern(random_items());
-        start_button.classList.toggle('game__btn--disabled');
-        restart_button.classList.toggle('game__btn--disabled');
-        items_container.classList.toggle('game__board--disabled');
-        btn_difficulty.classList.toggle('game__btn--disabled');
+        correctMarker();
+        btnVisibility();
     }
 });
 
@@ -55,12 +71,9 @@ restart_button.addEventListener('click', () => {
         return;
     }
     else {
-        game_status = 1;
+        game_status = 0;
         new_pattern(all_items);
-        start_button.classList.toggle('game__btn--disabled');
-        restart_button.classList.toggle('game__btn--disabled');
-        items_container.classList.toggle('game__board--disabled');
-        btn_difficulty.classList.toggle('game__btn--disabled');
+        btnVisibility();
     }
 });
 
@@ -69,53 +82,10 @@ game_container.addEventListener('click', e => {
     if (game_status === 1) {
 
         if (e.target.classList.contains('game__item')) {
-
-            let empty_field_position = current_items.indexOf(9);
             let clicked = current_items.indexOf(Number(e.target.innerText));
-            let switch_allowed = [];
+            grid_positon(clicked);
+            correctMarker();
 
-            // console.log('clicked: ' + clicked + ' - empty_field_position: ' + empty_field_position);
-
-            if (empty_field_position === 0) {
-                switch_allowed.push(1, 3);
-            }
-            if (empty_field_position === 1) {
-                switch_allowed.push(0, 2, 4);
-            }
-            if (empty_field_position === 2) {
-                switch_allowed.push(1, 5);
-            }
-            if (empty_field_position === 3) {
-                switch_allowed.push(0, 4, 6);
-            }
-            if (empty_field_position === 4) {
-                switch_allowed.push(1, 3, 5, 7);
-            }
-            if (empty_field_position === 5) {
-                switch_allowed.push(2, 4, 8);
-            }
-            if (empty_field_position === 6) {
-                switch_allowed.push(3, 7);
-            }
-            if (empty_field_position === 7) {
-                switch_allowed.push(6, 4, 8);
-            }
-            if (empty_field_position === 8) {
-                switch_allowed.push(5, 7);
-            }
-            if (switch_allowed.includes(clicked)) {
-                [current_items[empty_field_position], current_items[clicked]] = [current_items[clicked], current_items[empty_field_position]];
-                new_pattern(current_items);
-            }
-
-            /*
-            current_items.forEach(item => {
-                if (item[i] == all_items[i]) {
-                    item[i].classList.add('game__item--correct');
-                }
-            });
-            */
-           
             if (all_items.toString() === current_items.toString()) {
 
                 game_status = 0;
---
+++ grid.js
@@ -0,0 +1,54 @@
+function grid_positon(clicked) {
+    let switch_allowed = [];
+    let empty_field_position = current_items.indexOf(9);
+
+    if (empty_field_position === 0) {
+        switch_allowed.push(1, 3);
+    }
+    else if (empty_field_position === 1) {
+        switch_allowed.push(0, 2, 4);
+    }
+    else if (empty_field_position === 2) {
+        switch_allowed.push(1, 5);
+    }
+    else if (empty_field_position === 3) {
+        switch_allowed.push(0, 4, 6);
+    }
+    else if (empty_field_position === 4) {
+        switch_allowed.push(1, 3, 5, 7);
+    }
+    else if (empty_field_position === 5) {
+        switch_allowed.push(2, 4, 8);
+    }
+    else if (empty_field_position === 6) {
+        switch_allowed.push(3, 7);
+    }
+    else if (empty_field_position === 7) {
+        switch_allowed.push(6, 4, 8);
+    }
+    else if (empty_field_position === 8) {
+        switch_allowed.push(5, 7);
+    }
+
+    //
+
+    let row_limit = all_items.length;
+    let col_limit = Math.sqrt(row_limit);
+
+    for (let i = 0; i < row_limit; i++) {
+        // var three = data[i] + data[i + 1] + data[i + 2];
+
+        if (((i + 1) % col_limit) === 0) {
+            console.log(i);
+        }
+    
+    }
+    
+    //
+
+    if (switch_allowed.includes(clicked)) {
+        [current_items[empty_field_position], current_items[clicked]] = [current_items[clicked], current_items[empty_field_position]];
+        new_pattern(current_items);
+    }
+
+}
