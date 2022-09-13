c9b9650a9ce55bd1252350f86b2c14c62c7daa20
louisschapitz <louis.schapitz@gmail.com>
2022-09-13 09:38:39 +0200

    Easy mode

--- game.js
+++ game.js
@@ -41,7 +41,7 @@ function new_pattern(items) {
 function random_items() {
     current_items = all_items.slice().sort((a, b) => 0.5 - Math.random());
 
-    // Debug, Easy
+    // Debug, Easy order
     // current_items = [1, 2, 3, 4, 5, 9, 7, 8, 6];
     // current_items = [1, 2, 3, 9, 5, 6, 7, 8, 4, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
